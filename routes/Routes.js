2ced2a836b64216fe33358ab95ee903c62af952f
evalyn1998 <83055868+evalyn1998@users.noreply.github.com>
2021-08-19 15:01:33 +0800

    Update Routes.js

--- routes/Routes.js
+++ routes/Routes.js
@@ -32,7 +32,7 @@ export default function Routes() {
     
       return (
         <NavigationContainer>
-          {user ? <AppNavigator /> : <AuthNavigator />}
+          {user ? <AuthNavigator /> : <AppNavigator />}
         </NavigationContainer>
       );
 }
