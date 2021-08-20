e4ab4562c71f9d4523cb495a430bac033ac85289
evalyn1998 <83055868+evalyn1998@users.noreply.github.com>
2021-08-20 17:54:40 +0800

    Update firebase.js

--- firebase/firebase.js
+++ firebase/firebase.js
@@ -4,15 +4,7 @@ import firebaseConfig from './firebaseConfig';
 import 'firebase/firestore';
 
 
-// const firebaseConfig = {
-//     apiKey: "AIzaSyD3Ers2fpZuN8TGBwsBwsCCe0h0yrW_RNE",
-//     authDomain: "smiletrade-530c9.firebaseapp.com",
-//     projectId: "smiletrade-530c9",
-//     storageBucket: "smiletrade-530c9.appspot.com",
-//     messagingSenderId: "195887314625",
-//     appId: "1:195887314625:web:40766670dc43412ba60640",
-//     measurementId: "G-TY4GQGSKXZ"
-//   };
+
 let app;
   if(!firebase.apps.length){
    app = firebase.initializeApp(firebaseConfig);
