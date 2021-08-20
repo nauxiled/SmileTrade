84b1866e7c87d574f7a3af0a0176fc9d9fa1ef20
evalyn1998 <83055868+evalyn1998@users.noreply.github.com>
2021-08-20 17:52:57 +0800

    Update firebase.js

--- firebase/firebase.js
+++ firebase/firebase.js
@@ -3,16 +3,6 @@ require('firebase/auth')
 import firebaseConfig from './firebaseConfig';
 
 
-// const firebaseConfig = {
-//     apiKey: "AIzaSyD3Ers2fpZuN8TGBwsBwsCCe0h0yrW_RNE",
-//     authDomain: "smiletrade-530c9.firebaseapp.com",
-//     projectId: "smiletrade-530c9",
-//     storageBucket: "smiletrade-530c9.appspot.com",
-//     messagingSenderId: "195887314625",
-//     appId: "1:195887314625:web:40766670dc43412ba60640",
-//     measurementId: "G-TY4GQGSKXZ"
-//   };
-
   if(!firebase.apps.length){
     firebase.initializeApp(firebaseConfig);
 }
